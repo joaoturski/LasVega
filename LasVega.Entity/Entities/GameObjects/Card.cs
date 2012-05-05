45193a2c14cfcc732237395b13d2cdbb34a93a88
joaoturski <joaoturski@gmail.com>
2012-05-05 02:50:30 -0300

    modelagem das skills e card
    Signed-off-by: joaoturski <joaoturski@gmail.com>

---
+++ LasVega.Entity/Entities/GameObjects/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LasVega.Entity
+{
+    public class Card
+    {
+        public uint IdCard { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime AcquisitionDate { get; private set; }
+        public ushort OriginalAttackPoints { get; private set; }
+        public short CurrentAttackPoints { get; private set; }
+
+        // TODO: CRIAR UMA COLLECTION PROPRIA, PARA EVITAR PROBLEMAS FUTUROS DE
+        // DESVIRTUAÇÃO DE RESPONSABILIDADES, POR PARTE DE FUTUROS NOVOS PROGRAMADORES DESAVISADOS...
+        public IList<ActiveSkill> ActiveSkills { get; private set; }
+        public IList<PassiveSkill> PassiveSkills { get; private set; }
+        public IList<BuffSkill> Buffs { get; private set; }
+        public IList<DebuffSkill> Debuffs { get; private set; }
+
+    }
+}
---
+++ LasVega.Entity/Entities/Skills/Skill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LasVega.Entity
+{
+    public class Skill
+    {
+        /// <summary>
+        /// Identificador único dessa skill no banco de dados.
+        /// Sobreescrever na classe derivada, para representar a skill com os valores randomicos unicos 
+        /// atribuídos à ela.
+        /// </summary>
+        protected virtual uint IdSkill { get; set; }
+
+        /// <summary>
+        /// Identificador da família da skill, pois alguns debuffs podem influenciar um tipo de skill, pouco importando 
+        /// os valores dos atributos gerados.
+        /// </summary>
+        public uint IdSkillBase { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
