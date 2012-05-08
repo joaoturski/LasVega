16d304e16ef527bde2e62b97e0cca1db303c5866
joaoturski <joaoturski@gmail.com>
2012-05-08 01:27:17 -0300

    Adicionado projeto de testes unitários para o LasVega.Controller

---
+++ LasVega.Controller.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LasVega.Controller.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
